ec35ca9c19772f012541b5095ae4261918b9f3f6
Erick Saavedra <esvdrah@gmail.com>
2022-02-07 20:23:56 -0500

    Add associations

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -5,6 +5,13 @@ class Meme < ApplicationRecord
   has_and_belongs_to_many :tags
   has_and_belongs_to_many  :voters, join_table: "memes_users", class_name: "User"
   has_many :comments
-  has_many :commentators_memes, through: :comments, source: :user
+  has_many :commentators, through: :comments, source: :user
 
+  # Enums
+  enum type: { image: "image", gif: "gif" }
+
+  # Validations
+  validates :title, presence: true, uniqueness: true
+  validates :type, presence: true
+  validates :url_source, presence: true, format: {whit: %r{^https?://.+(png|gif|jpg)$}, message: "Add a valid meme URL"}
 end
