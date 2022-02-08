509a1b880fe8550478f106bece7eaf4213b59322
Erick Saavedra <esvdrah@gmail.com>
2022-02-07 20:58:30 -0500

    Add counter cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :meme
-
-  # Associations
-
+  belongs_to :meme, counter_cache: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_233107) do
+ActiveRecord::Schema.define(version: 2022_02_08_014407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,11 +50,6 @@ ActiveRecord::Schema.define(version: 2022_02_05_233107) do
     t.bigint "tag_id", null: false
   end
 
-  create_table "memes_users", id: false, force: :cascade do |t|
-    t.bigint "meme_id", null: false
-    t.bigint "user_id", null: false
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -73,6 +68,11 @@ ActiveRecord::Schema.define(version: 2022_02_05_233107) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", id: :serial, force: :cascade do |t|
+    t.bigint "meme_id", null: false
+    t.bigint "user_id", null: false
   end
 
   add_foreign_key "comments", "memes"
