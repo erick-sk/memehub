549fcd8ab92d77542a6b8baf017517be11fba6d9
Erick Saavedra <esvdrah@gmail.com>
2022-02-13 13:19:03 -0500

    Add Home controller and view

---
+++ app/controllers/memes_controller.rb
@@ -0,0 +1,15 @@
+class MemesController < ApplicationController
+  def index
+    memes = Meme.all.order(created_at: :desc)
+    @memes_by_date = memes.all.group_by { |meme| meme.created_at.to_datetime }
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+end
